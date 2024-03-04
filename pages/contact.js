808d5f89051bf1a77c7617403460535caededd0e
yunghavy <kimutaij192@gmail.com>
2024-03-04 09:52:14 +0300

    minor

--- pages/contact.js
+++ pages/contact.js
@@ -41,7 +41,7 @@ function Contact() {
       <NextSeo {...pagesSEO.contact} />
       <Suspense fallback={<Loader />}>
         <ResponsiveNavbar />
-        <section style={{ scrollBehavior: 'unset' }}>
+        <section className={styles.scroll} style={{ scrollBehavior: 'unset' }}>
           <div className={styles.Neo}>
             <div className={styles.imageContainer}>
               <Image
@@ -60,6 +60,7 @@ function Contact() {
           </div>
          
         </section>
+        <TestContact />
         <Parallax
             blur={0}
             bgImage="/images/paralla2.webp"
@@ -77,7 +78,6 @@ function Contact() {
               </div>
             </div>
           </Parallax>
-        <TestContact />
         <Footer />
       </Suspense>
       <Analytics />
